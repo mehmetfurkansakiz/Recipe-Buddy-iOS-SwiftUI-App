64d5ac8d86630b858fd7f672bd6832093448388d
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2025-04-16 02:34:44 +0300

    Initial Commit

---
+++ Recipe Buddy/Recipe_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_BuddyApp.swift
+//  Recipe Buddy
+//
+//  Created by furkan sakız on 16.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
